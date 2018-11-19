33f9398db2d97107515a1b9a2e7a16bf828d82c0
author <author@15218803807@163.com>
2018-11-19 11:00:49 +0800

    优化mongoose

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ var app = express()
 var bodyParser = require('body-parser')
 var route = require('./serverRoute/route')
 var mongoose = require('mongoose')
-var Schema = mongoose.Schema
+
 
 // 连接数据库 指定数据库不需要存在，当你插入第一条数据之后就自动被创建出来
-mongoose.connect('mongoose://localhost/kong')
+// mongoose.connect('mongoose://localhost/kong', { useMongoClient: true })
+mongoose.connect('mongodb://localhost/kong')
+var Schema = mongoose.Schema
+
+
 // 设计文档结构
 // 字段名称就是结构中的属性名称
 // 约束的目的时为了保证数据的完整性，不要脏数据
@@ -28,10 +32,11 @@ var useSchema = new Schema({
 var Student = mongoose.model('Student', useSchema)
 
 var student = new Student({
-  'name': 'kong',
-  'passsword': '123456',
-  'yzm': '1234'
+  'name': 'kong2',
+  'passsword': '111111',
+  'yzm': '222222'
 })
+// 新增数据
 // student.save(function(err, ret){
 //   if(err){
 //     console.log('新增失败');
@@ -40,14 +45,30 @@ var student = new Student({
 //   console.log(ret)
 // })
 
+// 查询数据
 Student.find(function(err, ret){
   if(err){
-    console.log('新增失败');
+    console.log('查询失败');
     return false
   }
-  console.log(23424)
   console.log(ret)
 })
+
+//删除数据
+// Student.deleteOne({ 
+//   _id: '5bf185fa3f91c2d32cfb2131',
+// },function(err, ret){
+//   if(err){
+//     console.log('删除失败');
+//     return false
+//   }
+//   console.log(ret)
+// })
+
+// Student.updateOne({ name: 'kong01' }, { name: 'kong1' }, function(err, res) {
+//   // Updated at most one doc, `res.modifiedCount` contains the number
+//   // of docs that MongoDB updated
+// });
 
 // 开放静态服务资源
 app.use('/public/',express.static('./public/'))
